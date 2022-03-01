4486e24ee78bb5dca6d684cbfd455f3451943728
MinhThanh28 <dragonf4393123@gmail.com>
2022-03-01 10:00:24 +0700

    lo dai di nhanh main

--- WinFormsApp1/WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -41,9 +41,9 @@ namespace WinFormsApp1
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(94, 67);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 15);
+            this.label1.Size = new System.Drawing.Size(71, 15);
             this.label1.TabIndex = 0;
-            this.label1.Text = "UserName";
+            this.label1.Text = "UserName 1";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
@@ -51,9 +51,9 @@ namespace WinFormsApp1
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(94, 135);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(59, 15);
+            this.label2.Size = new System.Drawing.Size(68, 15);
             this.label2.TabIndex = 0;
-            this.label2.Text = "PassWord";
+            this.label2.Text = "PassWord 1";
             this.label2.Click += new System.EventHandler(this.label1_Click);
             // 
             // txtuser
